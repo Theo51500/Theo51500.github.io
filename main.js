4d4a163e7dbdd347a2925b4683ce1e915608fd66
Théo Rousseau <rousseau.theo51@gmail.com>
2022-05-19 10:47:52 +0200

    upload ArticlePreview

--- main.js
+++ main.js
@@ -1,32 +1,14 @@
-const text = document.querySelectorAll('.text');
-const li = document.querySelectorAll('.li-content');
-const title = document.querySelectorAll('.title');
+const share = document.querySelector('.share');
+const btn = document.querySelector('.button');
 
-function reset() {
-    for(let i = 0; i <= li.length - 1; i++) {
-        text[i].style.display = 'none';
-        title[i].style.fontWeight = 'lighter';
+btn.addEventListener('click', () => {
+    share.style.display = 'flex';
+})
+
+document.addEventListener('click', (e) => {
+    if(!e.target.classList.contains('share') 
+    && !e.target.classList.contains('button')
+    && !e.target.classList.contains('fa-share')){
+        share.style.display = 'none';
     }
-}
-
-for (let i = 0; i <= li.length - 1; i++) {
-    let none = true
-    li[i].addEventListener('click', () => {
-        if(none){
-            reset();
-            text[i].style.display = 'block';
-            title[i].style.fontWeight = 'bold';
-            none = false
-        } else {
-            text[i].style.display = 'none';
-            title[i].style.fontWeight = 'lighter';
-            none = true
-        }
-
-    })
-}
-
-
-
-
-
+})
--- style-guide.md
+++ style-guide.md
@@ -9,38 +9,26 @@ The designs were created to the following widths:
 
 ## Colors
 
-### Primary
-
-#### Text
-
-- Very dark desaturated blue: hsl(238, 29%, 16%)
-- Soft red: hsl(14, 88%, 65%)
-
-#### Gradient
-
-Background gradient:
-
-- Soft violet: hsl(273, 75%, 66%)
-- Soft blue: hsl(240, 73%, 65%)
-
-### Neutral
-
-#### Text
-
-- Very dark grayish blue: hsl(237, 12%, 33%)
-- Dark grayish blue: hsl(240, 6%, 50%)
-
-#### Dividers
-
-- Light grayish blue: hsl(240, 5%, 91%)
+- Very Dark Grayish Blue: hsl(217, 19%, 35%)
+- Desaturated Dark Blue: hsl(214, 17%, 51%)
+- Grayish Blue: hsl(212, 23%, 69%)
+- Light Grayish Blue: hsl(210, 46%, 95%)
 
 ## Typography
 
 ### Body Copy
 
-- Font size: 12px
+- Font size: 13px
 
-### Font
+### Headings
 
-- Family: [Kumbh Sans](https://fonts.google.com/specimen/Kumbh+Sans)
-- Weights: 400, 700
+- Family: [Manrope](https://fonts.google.com/specimen/Manrope)
+- Weights: 500, 700
+
+## Icons
+
+We provide SVGs for the social icons. But please feel free to use a font icon library if you like. Some suggestions can be found below:
+
+- [Font Awesome](https://fontawesome.com)
+- [IcoMoon](https://icomoon.io)
+- [Ionicons](https://ionicons.com)
